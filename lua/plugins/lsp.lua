ae611986a3c2037577ce47b093f87fff571c8cda
Jalmari Vattulainen <jalmari.vattulainen@digia.com>
2026-02-09 12:12:35 +0200

    Ensure installed basedpyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,49 +1,50 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "basedpyright",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.lsp.handlers["textDocument/hover"] =
-				vim.lsp.with(vim.lsp.handlers.hover, { max_width = 120, max_height = 60, border = "rounded" })
+      vim.lsp.handlers["textDocument/hover"] =
+          vim.lsp.with(vim.lsp.handlers.hover, { max_width = 120, max_height = 60, border = "rounded" })
 
-			vim.lsp.enable("lua_ls")
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = {
-							-- Get rid of 'undefined global vim' warning
-							globals = { "vim" },
-						},
-					},
-				},
-				capabilities = capabilities,
-			})
-			vim.lsp.enable("ts_ls")
-			vim.lsp.config("ts_ls", {
-				capabilities = capabilities,
-			})
-			vim.lsp.enable("basedpyright")
-			vim.lsp.config("basedpyright", {
-				capabilities = capabilities,
-			})
-		end,
-	},
+      vim.lsp.enable("lua_ls")
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get rid of 'undefined global vim' warning
+              globals = { "vim" },
+            },
+          },
+        },
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("ts_ls")
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("basedpyright")
+      vim.lsp.config("basedpyright", {
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
